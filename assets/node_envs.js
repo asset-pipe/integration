4bccd0a5605e42e82c15492db83c96f27f34431c
Richard Walker <digitalsadhu@gmail.com>
2018-02-06 09:45:24 +0100

    test: add tests for env replacement and DCE

--- assets/node_envs.js
+++ assets/node_envs.js
@@ -1,3 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
-    console.log('In prod!');
+    console.log('In prod!!');
+} else if (process.env.NODE_ENV === 'development') {
+    console.log('In dev!!');
+} else {
+    console.log('In limbo!!');
 }
